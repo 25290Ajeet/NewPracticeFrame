975e0787421d3ec6b6f696d37fe87dbc263d8dcb
Ajeet Sharma <ajeetsharma@Ajeets-Mac-mini.local>
2019-11-21 15:13:09 +0530

    Initial Commit

---
+++ NewPracticeFrame/NewPracticeFrame.h
@@ -0,0 +1,19 @@
+//
+//  NewPracticeFrame.h
+//  NewPracticeFrame
+//
+//  Created by Ajeet Sharma on 21/11/19.
+//  Copyright © 2019 Telus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewPracticeFrame.
+FOUNDATION_EXPORT double NewPracticeFrameVersionNumber;
+
+//! Project version string for NewPracticeFrame.
+FOUNDATION_EXPORT const unsigned char NewPracticeFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewPracticeFrame/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '12.2'
+
+target 'NewPracticeFrame' do
+    pod 'MBProgressHUD', '~> 1.1.0', :modular_headers => true
+
+end
